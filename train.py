d79b8727b138708fc6c1cb9029c9686ca866c3d6
Fabio Frangiamore <f.frangiamore@gmail.mcom>
2020-12-14 18:35:50 +0100

    first examples

--- train.py
+++ train.py
@@ -5,6 +5,10 @@ from torch.utils.tensorboard import SummaryWriter
 from utils.utils import generate_patch
 from matplotlib import pyplot as plt
 from model.unet_model import UNet
+import tqdm
+import torch.nn as nn
+from torch import optim
+from model.loss import DiceLoss
 
 DIR_IMG = 'data/'
 DIR_CHECKPOINT = 'checkpoints/'
@@ -19,7 +23,10 @@ def train_net(batch_size = 1, lr = 0.001):
     writer = SummaryWriter(comment=f'LR_{lr}_BS_{batch_size}')
 
     net = UNet(1, 1, bilinear=False)
+    criterion = DiceLoss()
+    optimizer = optim.RMSprop(net.parameters(), lr=lr, weight_decay=1e-8, momentum=0.9)
 
+    epoch_loss = 0
     for i in train_loader:
         img = i['ct']['data'][0]
         mask = i['segm']['data'][0]
@@ -30,9 +37,22 @@ def train_net(batch_size = 1, lr = 0.001):
         plt.imshow(patch_img[5][:,:, 20],cmap='gray')
         plt.show()
 
+
+
         print(patch_img.unsqueeze(1).shape)
 
-        mask_pred = net(patch_img.unsqueeze(1)[0:1])       
+        for j in range(0,len(patch_img)-1):
+            #mask_pred = net(patch_img.unsqueeze(1)[j:j+1])  
+            plt.imshow(patch_img[j][:,:, 20],cmap='gray')
+            plt.show()
+            plt.imshow(patch_mask[0][:,:, 20],cmap='gray')
+            plt.show()
+            #loss = criterion(mask_pred, patch_mask[j:j+1].long())   
+            #print(loss.item())  
+            #optimizer.zero_grad()
+            #loss.backward()
+
+            #optimizer.step()
 
 
 if __name__ == '__main__':
